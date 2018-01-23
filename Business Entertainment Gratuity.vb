2b2294d4baf59d6cb59d955c664d2bc46e863a12
Ryan <ryanjoya@users.noreply.github.com>
2018-01-23 13:59:53 -0600

    Update Business Entertainment Gratuity.vb

--- Business Entertainment Gratuity.vb
+++ Business Entertainment Gratuity.vb
@@ -1,9 +1,15 @@
-=IF(B3="Domestic",IF(_Fund=70,"GSA maximum",(
-IF(OR(_Fund=10,_Fund=90),"No","20% of subtotal maximum")
+=IF(B3 = "Domestic", 
 
-)),
-IF(B3<>"FOREIGN","",
+   IF ( _Fund = 70,  "GSA maximum",  
+	(
+		IF(OR(_Fund=10, _Fund=90),"No", "20% of subtotal maximum")
 
-IF(OR(_Fund=10,_Fund=70),"Not Included",
-IF(_Fund=90,"No","20% of subtotal maximum")))
-)
+		))
+
+ ,
+   IF (B3 <> "Foreign", "" ,
+
+	IF(OR(_Fund=10, _Fund=70),"Not Included",
+		
+		IF(_Fund=90, "No", "20% of subtotal maximum")))
+   )
